b0b10367babc76965e5e8091a9c6c4895493de7d
julianrakhuba <83831752+julianrakhuba@users.noreply.github.com>
2023-10-26 10:31:04 -0700

    Cleanup

--- iCalc/src/application/Indicators.java
+++ iCalc/src/application/Indicators.java
@@ -1,6 +1,5 @@
 
 /*******************************************************************************
- * /*******************************************************************************
  *  *  MIT License
  *  *
  *  *
@@ -19,7 +18,6 @@
  *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING
  *  * FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  *  * DEALINGS IN THE SOFTWARE.
- *  *******************************************************************************/
  *******************************************************************************/
 
 package application;
